0bcc821fc7a0ea81d6ef35964e5312c96c46d2f0
MuneefDS <muneefullah24mdswe363@gmail.com>
2026-02-23 09:47:01 +0500

    Add files via upload

---
+++ Simple_Calculator_User_Greeting.py
@@ -0,0 +1,15 @@
+Name=str(input("Enter your name? "))
+print("hello",Name)
+Num1=int(input("Enter first number? "))
+Num2=int(input("Enter second number? "))
+sum=Num1+Num2
+print("Addition",sum)    
+sub=Num1-Num2
+print("subtraction",sub)
+mul=Num1*Num2
+print("multiplication",mul)
+if Num2==0:
+  print ("num2 is not divisible by zero")
+else:  
+ div=Num1/Num2
+ print("Division",div)
